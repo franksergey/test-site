ae1d2562343218184927a2bb1e13da60beb9b8f2
Soucelover <mr.ktotov@gmail.com>
2026-04-26 08:30:54 +0300

    Implement asynchronous loop factory

--- src/server/__main__.py
+++ src/server/__main__.py
@@ -1,8 +1,27 @@
+import asyncio
 import logging
+import sys
 
 from server.logging import setup_logging
 
 logger = logging.getLogger(__name__)
+
+
+def create_asyncio_event_loop() -> asyncio.AbstractEventLoop:
+    if sys.platform == "win32":
+        import winloop  # noqa: PLC0415
+
+        logger.info("Используется цикл событий winloop.Loop")
+        return winloop.new_event_loop()
+
+    if sys.platform == "linux":
+        import uvloop  # noqa: PLC0415
+
+        logger.info("Используется цикл событий uvloop.Loop")
+        return uvloop.new_event_loop()
+
+    logger.info("Выбор цикла событий предоставляется asyncio")
+    return asyncio.new_event_loop()
 
 
 def launch_server() -> None:
